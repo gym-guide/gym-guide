1c6190b6cd068c51a5a46b15917668b0fcefc5f1
Artemii Pavlenko <artemiipav@gmail.com>
2023-06-07 18:12:21 +0300

    Linter added, prepered empty templay for front-end

--- front-end/src/main.tsx
+++ front-end/src/main.tsx
@@ -1,10 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import { HashRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+const APP_MOUNTING_ROOT = document.getElementById('root');
+
+if (APP_MOUNTING_ROOT == null) {
+  throw new Error('React mounting root is not found');
+}
+
+ReactDOM.createRoot(APP_MOUNTING_ROOT).render(
   <React.StrictMode>
-    <App />
+    <HashRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </HashRouter>
   </React.StrictMode>,
-)
+);
